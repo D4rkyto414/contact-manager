09a0d73cde01a48aebd817c63a9980b23a47c395
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-08-30 19:51:24 -0500

    Create contacts.js

---
+++ src/data/contacts.js
@@ -0,0 +1,5 @@
+export const contactsData = [
+  { id: 1, name: "Paul Perez", phone: "944238940", favorite: false },
+  { id: 2, name: "Belisa Cabanillas", phone: "989196164", favorite: true },
+  { id: 3, name: "Camila Velasquez", phone: "917683075", favorite: false }
+];
